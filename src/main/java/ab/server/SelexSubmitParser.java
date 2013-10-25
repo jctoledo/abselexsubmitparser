bda25a2056bea764d9ab7498d3725a1d3ed04fed
Jose Cruz-Toledo <josemiguelcruztoledo@gmail.com>
2013-10-25 10:08:03 -0400

    fixed pmid, doi and reference parsing

--- src/main/java/ab/server/SelexSubmitParser.java
+++ src/main/java/ab/server/SelexSubmitParser.java
@@ -41,7 +41,6 @@ import ab.shared.lib.AptamerTarget;
 import ab.shared.lib.Interaction;
 import ab.shared.lib.SelexExperiment;
 
-
 /**
  * @author Jose Cruz-Toledo
  * 
@@ -52,102 +51,149 @@ public class SelexSubmitParser extends HttpServlet {
 	 * 
 	 */
 	private static final long serialVersionUID = 5714015034095536468L;
+
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp){
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
 		Map<String, String[]> rm = req.getParameterMap();
 		doGet(req, resp);
 	}
-	
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
 		JSONObject rm = new JSONObject();
 		resp.setContentType("text/json");
 		PrintWriter out = null;
-		String in= null;
+		String in = null;
 		String fn = null;
 		try {
 			out = resp.getWriter();
 			in = req.getParameter("se");
 			fn = req.getParameter("fn");
-			if (in != null && fn !=null) {
-				JSONObject jo = new JSONObject(in);			
-				AptamerBaseSelexSubmitParser abssp = new AptamerBaseSelexSubmitParser(jo);
-				//now convert the AptamerBaseSelexSubmitParser into a write query?
+			if (in != null && fn != null) {
+				JSONObject jo = new JSONObject(in);
+				AptamerBaseSelexSubmitParser abssp = new AptamerBaseSelexSubmitParser(
+						jo);
+				// now convert the AptamerBaseSelexSubmitParser into a write
+				// query?
 				SelexExperiment se = abssp.getSelexExperiment();
-				if(se != null){
+				if (se != null) {
 					int aPmid = se.getPmid();
-					//now check that the experiment has a pmid
-					if(aPmid >1 ){
-						//first check if the PMID exists in freebase 
-						String mid = FreebaseHelper.checkPmid(aPmid);
-						if(mid == null){
-							//start preparing the output
-							//now check the interactions
-							JSONArray interactions_arr = new JSONArray();
-							List<Interaction> interactions = abssp.getInteractions();
-							for (Interaction anInteraction : interactions) {
-								//verify that each interaction has the following things
-								//an aptamer target
-								AptamerTarget at = anInteraction.getAptamer_target();
-								if(at.getName().length() == 0){
-									out.println("No target name provided. Please check submission :"+ fn);
-								}
-								//some affinityExperiments
-								List<AffinityExperiment> ae_list = anInteraction.getAffinityExperiments();
-								if(ae_list.size() == 0){
-									out.println("No affinity experiments provided. Please check submission : "+ fn);
-								}
-								//check that all aptamers have types
-								List<Aptamer> a_list = anInteraction.getAptamers();
-								if(a_list.size() == 0){
-									out.println("No aptamers provided. Please check submission : "+ fn);
-								}else{
-									for(Aptamer anApt: a_list){
-										if(anApt.getPolymerType().length() == 0){
-											out.println("No polymer type provided. Please check submission: "+ fn);
-										}
-									}
-								}
-								//now get the json version for these interactions
-								JSONObject j = anInteraction.getJSONObject();
-								interactions_arr.put(j);
-							}//for interactions
-						
-							
-							rm.put("\"se\"", se.getJSONObject());
-							
-							rm.put("\"interactions\"", interactions_arr);
-							
-							out.println(rm);
-						}else{
-							out.println("PMID already in Freebase. Please check submission :"+fn);
-						}
-					}else{
-						out.println("Invalid PMID provided : "+ aPmid + " for file : "+fn);
-					}
-				}else{
-					//no PMID so check that there is a doi or a reference
 					String doi = se.getDoi();
 					String ref = se.getReference();
-					if(doi != null){
-						
+					// now check that the experiment has a pmid
+					if (aPmid > 1) {
+						// first check if the PMID exists in freebase
+						String mid = FreebaseHelper.checkPmid(aPmid);
+						if (mid == null) {
+							// start preparing the output
+							// now check the interactions
+							try {
+								JSONArray interactions_arr = this
+										.checkInteractions(abssp, fn);
+								rm.put("\"se\"", se.getJSONObject());
+								rm.put("\"interactions\"", interactions_arr);
+								out.println(rm);
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						} else {
+							out.println("PMID already in Freebase. Please check submission :"
+									+ fn);
+						}
+					} else if (doi != null) {
+						// check if the doi exists in freebase
+						String mid = FreebaseHelper.checkDoi(doi);
+						if (mid == null) {
+							try {
+								JSONArray interactions_arr = this
+										.checkInteractions(abssp, fn);
+								rm.put("\"se\"", se.getJSONObject());
+								rm.put("\"interactions\"", interactions_arr);
+								out.println(rm);
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						} else {
+							out.println("DOI already in Freebase. Please check submission :"
+									+ fn);
+						}
+					} else if (ref != null) {
+						// check if ref exists in freebase
+						String mid = FreebaseHelper.checkReference(ref);
+						if (mid == null) {
+							try {
+								JSONArray interactions_arr = this
+										.checkInteractions(abssp, fn);
+								rm.put("\"se\"", se.getJSONObject());
+								rm.put("\"interactions\"", interactions_arr);
+								out.println(rm);
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						} else {
+							out.println("This reference is already in Freebase. Please check submission :"
+									+ fn);
+						}
+					} else {
+						out.println("No reference details provided. Please check submission: "
+								+ fn);
 					}
 				}
-				
-			}else{
+			} else {
 				out.println("Please provide a selex experiment JSON and a filename using the 'se' and 'fn' parameters respectively");
 			}
-		}catch (JSONException e) {
+		} catch (JSONException e) {
 			out.println("INVALID JSON");
 			out.flush();
 			e.printStackTrace();
-		} catch(NullPointerException e){
+		} catch (NullPointerException e) {
 			out.println("INVALID JSON error 2");
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
-		} 
+		}
 	}
-	
-	
+
+	protected JSONArray checkInteractions(AptamerBaseSelexSubmitParser abssp,
+			String aFn) throws Exception {
+		List<Interaction> interactions = abssp.getInteractions();
+		JSONArray rm = new JSONArray();
+		for (Interaction anInteraction : interactions) {
+			// verify that each interaction has the following things
+			// an aptamer target
+			AptamerTarget at = anInteraction.getAptamer_target();
+			if (at.getName().length() == 0) {
+				throw new Exception(
+						"No target name provided. Please check submission :"
+								+ aFn);
+			}
+			// some affinityExperiments
+			List<AffinityExperiment> ae_list = anInteraction
+					.getAffinityExperiments();
+			if (ae_list.size() == 0) {
+				throw new Exception(
+						"No affinity experiments provided. Please check submission : "
+								+ aFn);
+			}
+			// check that all aptamers have types
+			List<Aptamer> a_list = anInteraction.getAptamers();
+			if (a_list.size() == 0) {
+				throw new Exception(
+						"No aptamers provided. Please check submission : "
+								+ aFn);
+			} else {
+				for (Aptamer anApt : a_list) {
+					if (anApt.getPolymerType().length() == 0) {
+						throw new Exception(
+								"No polymer type provided. Please check submission: "
+										+ aFn);
+					}
+				}
+			}
+			// now get the json version for these interactions
+			JSONObject j = anInteraction.getJSONObject();
+			rm.put(j);
+		}
+		return rm;
+	}
+
 }
